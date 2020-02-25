4b0b75f58bf87dc7c7a73dc2fa854e7d9de7e231
Hank <asdfzxcv7821@gmail.com>
2020-02-25 17:35:17 +0800

    login in test

--- src/main/java/com/example/demo/webConfig.java
+++ src/main/java/com/example/demo/webConfig.java
@@ -3,12 +3,13 @@ package com.example.demo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
-@EnableWebMvc
-public class webConfig extends WebMvcConfigurerAdapter {
+public class webConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -22,6 +23,14 @@ public class webConfig extends WebMvcConfigurerAdapter {
                         "classpath:/static/dist/",
                         "classpath:/static/css/",
                         "classpath:/static/js/");
+        
     }
+    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
 
 }
